e08f48b3714538b65b355968d3878cd82612e04c
feliperamosgarcia <77219996+feliperamosgarcia@users.noreply.github.com>
2022-07-23 14:13:28 -0300

    Teste Sirius

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,11 @@ def load_data(nrows):
 
 
 st.subheader('Raw data')
-
+st.write(data)
 
 st.subheader('Number of pickups by hour')
 
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 
 st.bar_chart(hist_values)
